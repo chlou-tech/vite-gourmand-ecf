51b3138d4018e2bd1e43bd8569df02e3b1f424dd
Chloe <chloe.cerisier23@gmail.com>
2026-04-14 15:45:38 +0200

    fix sql path

--- public/import.php
+++ public/import.php
@@ -3,7 +3,7 @@
 require_once '../config/database.php';
 
 try {
-    $sql = file_get_contents('database.sql');
+    $sql = file_get_contents('../database.sql');
     $pdo->exec($sql);
 
     echo "✅ Import réussi !";
