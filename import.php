9baa64283c71511f704974607110b6c06c9fa00c
Chloe <chloe.cerisier23@gmail.com>
2026-04-14 14:39:34 +0200

    fix: export data SQL

---
+++ import.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'config/database.php';
+
+try {
+    $sql = file_get_contents('database.sql');
+    $pdo->exec($sql);
+
+    echo "✅ Import réussi !";
+} catch (PDOException $e) {
+    echo "❌ Erreur : " . $e->getMessage();
+}
+
